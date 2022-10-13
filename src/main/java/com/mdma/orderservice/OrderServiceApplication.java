5cd29cfd456217d01082121878dd51ef64e01148
David Chang <Davidchang@live.nl>
2022-10-13 13:06:27 +0200

    Fixed deployment config and added sql seeder at startup

--- src/main/java/com/mdma/orderservice/OrderServiceApplication.java
+++ src/main/java/com/mdma/orderservice/OrderServiceApplication.java
@@ -1,7 +1,17 @@
 package com.mdma.orderservice;
 
+import com.mdma.orderservice.Enum.OrderStatus;
+import com.mdma.orderservice.Enum.ProductSize;
+import com.mdma.orderservice.Model.Order;
+import com.mdma.orderservice.Model.Product;
+import com.mdma.orderservice.Repository.OrderRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class OrderServiceApplication {
@@ -11,23 +21,23 @@ public class OrderServiceApplication {
     }
 
 
-//    @Bean
-//    CommandLineRunner runner(RestaurantRepository repository) {
-//        return args ->  {
-//            Restaurant restaurant = new Restaurant(
-//                "Bread de bakker",
-//                new Menu(
-//                    new ArrayList<Product>(
-//                        Arrays.asList(
-//                            new Product("Kaas soep", "Lekker kaas soepje", 4.50, ProductSize.Small, true),
-//                            new Product("Kaas soep", "Lekker kaas soepje", 4.50, ProductSize.Small, true)
-//                        )
-//                    )
-//                )
-//            );
-//
-//            repository.insert(restaurant);
-//        };
-//    }
+    @Bean
+    CommandLineRunner runner(OrderRepository repository) {
+        return args ->  {
+            Order order = new Order(
+                    "NewOrder1",
+                    "1",
+                    new ArrayList<Product>(
+                            Arrays.asList(
+                                    new Product("Kaas", "Lekker stukje kaas", 5.50, ProductSize.Small, true),
+                                    new Product("Kaas soep", "Lekker kaas soepje", 6.50, ProductSize.Small, true))
+
+                    ),
+                    12.00,
+                OrderStatus.Received);
+
+            repository.insert(order);
+        };
+    }
 
 }
