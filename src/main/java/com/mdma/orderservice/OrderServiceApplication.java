805a314f5fcd9b325cef1652111de996ae25c875
David Chang <Davidchang@live.nl>
2022-10-13 13:29:55 +0200

    removed seeder and added api documentation

--- src/main/java/com/mdma/orderservice/OrderServiceApplication.java
+++ src/main/java/com/mdma/orderservice/OrderServiceApplication.java
@@ -5,6 +5,7 @@ import com.mdma.orderservice.Enum.ProductSize;
 import com.mdma.orderservice.Model.Order;
 import com.mdma.orderservice.Model.Product;
 import com.mdma.orderservice.Repository.OrderRepository;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,26 +19,6 @@ public class OrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
-    }
-
-
-    @Bean
-    CommandLineRunner runner(OrderRepository repository) {
-        return args ->  {
-            Order order = new Order(
-                    "NewOrder1",
-                    "1",
-                    new ArrayList<Product>(
-                            Arrays.asList(
-                                    new Product("Kaas", "Lekker stukje kaas", 5.50, ProductSize.Small, true),
-                                    new Product("Kaas soep", "Lekker kaas soepje", 6.50, ProductSize.Small, true))
-
-                    ),
-                    12.00,
-                OrderStatus.Received);
-
-            repository.insert(order);
-        };
     }
 
 }
