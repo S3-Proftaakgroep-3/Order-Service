e4955ded01589d08554d8fa9094d410acd154736
David Chang <Davidchang@live.nl>
2022-11-10 13:19:16 +0100

    Changed OrderControllerTest to not be public

--- src/test/java/controller/OrdercontrollerTest.java
+++ src/test/java/controller/OrdercontrollerTest.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
-public class OrderControllerTest {
+class OrderControllerTest {
 
     @Mock
     private OrderRepository orderRepository;
