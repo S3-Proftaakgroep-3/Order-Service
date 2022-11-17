78fe55b6c90c5cfdb68cd7562ccf050246177616
DavidChang <57895305+Davidchang24@users.noreply.github.com>
2022-11-17 15:02:53 +0100

    Update OrderRepository.java
    remove redundant

--- src/main/java/com/mdma/orderservice/Repository/OrderRepository.java
+++ src/main/java/com/mdma/orderservice/Repository/OrderRepository.java
@@ -9,6 +9,4 @@ import java.util.List;
 public interface OrderRepository extends MongoRepository<Order, String> {
     List<Order> findOrdersByRestaurantIdAndTableId(String restaurantId, String tableId);
     List<Order> findOrdersByRestaurantId(String restaurantId);
-
-    List<Order> findOrdersByRestaurantId(String restaurantId);
 }
